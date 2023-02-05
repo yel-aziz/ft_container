348578be7f9c236efd85b767e66a2ccbb47b7516
Yassir El azizi <yel-aziz@e2r4p1.1337.ma>
2023-02-06 00:43:50 +0100

    insert done ther is more work to do in it but the main algo is done

--- vector/main.cpp
+++ vector/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yel-aziz <yel-aziz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 15:12:47 by yel-aziz          #+#    #+#             */
-/*   Updated: 2023/02/05 22:57:37 by yel-aziz         ###   ########.fr       */
+/*   Updated: 2023/02/06 00:32:29 by yel-aziz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,14 @@
 int main()
 {
    // std::vector<int> a;
+   // std::vector<int>::iterator it;
+
+   //  a.push_back(9);
+   // a.push_back(9);
+   // it = a.begin();
+
+   // std::cout << " size == " <<  a.diff(a.end(),it) << "\n";
+
 
    // a.push_back(9);
    // a.insert(a.begin(),22);
@@ -30,11 +38,15 @@ int main()
    ft::vector<int>::iterator it;
 
    b.push_back(9);
+   b.push_back(9);
    it = b.begin();
+   
+   b.insert(it,555);
    // int a = *it;
    
    // b.insert(it,22);
   
    // b.insert(b.begin(),22);
-   std::cout << " size == " <<  b.diff(b.begin(),it);
+   std::cout << " size == " << b[0];
+   // std::cout << " size == " <<  b[2];
 }
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: yel-aziz <yel-aziz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 14:58:22 by yel-aziz          #+#    #+#             */
-/*   Updated: 2023/02/05 23:38:30 by yel-aziz         ###   ########.fr       */
+/*   Updated: 2023/02/06 00:32:22 by yel-aziz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,52 +18,60 @@
 namespace ft
 {
     template <class T, class Allocator = std::allocator<T> >
-class vector
-{
+    class vector
+    {
     public:
-        typedef Allocator                                   allocator_type;
-        typedef size_t                                      size_type;
-        typedef T                                           value_type;
-        typedef ft::random_access_iterator<value_type>      iterator;
-        typedef ft::random_access_iterator<value_type>      const_iterator;
-        typedef ptrdiff_t                                   difference_type;
+        typedef Allocator allocator_type;
+        typedef size_t size_type;
+        typedef T value_type;
+        typedef ft::random_access_iterator<value_type> iterator;
+        typedef ft::random_access_iterator<value_type> const_iterator;
+        typedef ptrdiff_t difference_type;
 
         // typedef ft::reverse_iterators<iterator>             reverse_iterator;
         // typedef ft::reverse_iterators<const_iterator>       const_reverse_iterator;
-        value_type      *array;
+        value_type *array;
+
     private:
-        allocator_type  myallocator;
-        size_type       capacity_;
-        size_type       size_;
+        allocator_type myallocator;
+        size_type capacity_;
+        size_type size_;
         /////////////////////////////////////////// realocation methode ////////////////////////////////////////////////////////
         value_type *realocation(value_type *oldarray, size_type numbertoalocate)
         {
-            value_type  *tmp;
-            int         i;
+            value_type *tmp;
+            int i;
 
             tmp = myallocator.allocate(numbertoalocate);
             i = -1;
             while (++i <= capacity_)
                 tmp[i] = oldarray[i];
-                
+
             this->capacity_ = numbertoalocate;
             return (tmp);
         }
+
     public:
-    difference_type diff(iterator it1, iterator it2){
-        return it1 - it2;
-    }
-    ///////////////////////////////////////////// constructors /////////////////////////////////////////////////////////////
-        vector (const allocator_type& alloc = allocator_type()): myallocator(alloc), capacity_(0), size_(0){}
-        vector (const vector& x){ this->array = x.array; this->capacity_ = x.capacity_; this->size_ = x.size_;}
-        vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()): myallocator(alloc)
+        difference_type diff(iterator it1, iterator it2)
+        {
+            return it1 - it2;
+        }
+        ///////////////////////////////////////////// constructors /////////////////////////////////////////////////////////////
+        vector(const allocator_type &alloc = allocator_type()) : myallocator(alloc), capacity_(0), size_(0) {}
+        vector(const vector &x)
+        {
+            this->array = x.array;
+            this->capacity_ = x.capacity_;
+            this->size_ = x.size_;
+        }
+        vector(size_type n, const value_type &val = value_type(), const allocator_type &alloc = allocator_type()) : myallocator(alloc)
         {
             this->array = myallocator.allocate(n);
-            for(int i = 0; i <= (n - 1); i++)
+            for (int i = 0; i <= (n - 1); i++)
                 this->array[i] = val;
         }
-        ~vector(){myallocator.destroy(this->array);}
-    ////////////////////////////////////////// operators overloding ////////////////////////////////////////////////////////
+        ~vector() { myallocator.destroy(this->array); }
+        ////////////////////////////////////////// operators overloding ////////////////////////////////////////////////////////
         vector &operator=(const vector &obj)
         {
             if (this == &obj)
@@ -71,47 +79,74 @@ class vector
             this->array = obj.array;
             return *this;
         }
-        const value_type& operator[](std::size_t index) const
+        const value_type &operator[](std::size_t index) const
         {
             return this->array[index];
         }
-    //////////////////////////////////////// insertion methodes ////////////////////////////////////////////////////////////
-        void push_back (const value_type& val)
+        //////////////////////////////////////// insertion methodes ////////////////////////////////////////////////////////////
+        void push_back(const value_type &val)
         {
-            if (capacity_ == 0) { array = myallocator.allocate(1); capacity_ = 1;}
-            else if (capacity_ == size_){ array = realocation(array, (capacity_ * 2));}
-            array[size_] = val; size_++;
+            if (capacity_ == 0)
+            {
+                array = myallocator.allocate(1);
+                capacity_ = 1;
+            }
+            else if (capacity_ == size_)
+            {
+                array = realocation(array, (capacity_ * 2));
+            }
+            array[size_] = val;
+            size_++;
         }
-        
+
         void pop_back()
         {
             myallocator.destroy((array + (size_ - 1)));
             size_--;
         }
 
-        // iterator insert(const_iterator pos, const T& value )
-        // {
-        // }
-        
-        void assign (size_type n, const value_type& val)
+        iterator insert(const_iterator pos, const T &value)
+        {
+            if ((1 + size_) > this->capacity_)
+            {
+                int i = -1;
+                value_type *new_tab = myallocator.allocate((1 + size_));
+                while (++i <= capacity_)
+                {
+                    if (i == diff(end(),pos) - 1){ new_tab[i] = value; i++;}
+                    new_tab[i] = this->array[i];
+                }
+                // myallocator.deallocat(this->array,capacity_);
+                this->array = new_tab;
+                // std::cout << new_tab[0];
+                // std::cout << new_tab[1];
+                // std::cout << new_tab[2];
+            }
+            return end();
+        }
+
+        void assign(size_type n, const value_type &val)
         {
             int i = -1;
-            if(capacity_ <= n) {this->array = myallocator.allocate(n);}
+            if (capacity_ <= n)
+            {
+                this->array = myallocator.allocate(n);
+            }
             while (++i <= n)
                 this->array[i] = val;
             size_ = (i - 1);
             capacity_ = n;
         }
-    /////////////////////////////////////////////// iterators //////////////////////////////////////////////////////////////
-        iterator begin(){ return(iterator(array));}
-        const_iterator begin() const {return(iterator(array));}
-        const_iterator end() const{return(iterator(array + size_));}
-        iterator end(){return(iterator(array + size_));}
+        /////////////////////////////////////////////// iterators //////////////////////////////////////////////////////////////
+        iterator begin() { return (iterator(array)); }
+        const_iterator begin() const { return (iterator(array)); }
+        const_iterator end() const { return (iterator(array + size_)); }
+        iterator end() { return (iterator(array + size_)); }
         // reverse_iterator rbegin(){return(this->array[size_]);}
         // const_reverse_iterator rbegin() const {return(this->array[size_]);}
         // reverse_iterator rend(){return(this->array[0]);}
         // const_reverse_iterator rend() const{return(this->array[0]);}
-    ///////////////////////////////////////////// size /////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////// size /////////////////////////////////////////////////////////////////////
         size_type size() const
         {
             return this->size_;
@@ -120,15 +155,14 @@ class vector
         {
             return this->capacity_;
         }
-        size_type max_size() const { return this->capacity_;}
+        size_type max_size() const { return this->capacity_; }
         // void resize (size_type n, value_type val = value_type()){
-            
+
         // }
-        
-    // value_type operator[](size_type n){ return this->array[n];}
-};
+
+        // value_type operator[](size_type n){ return this->array[n];}
+    };
 
 }
-
 
 #endif
